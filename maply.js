7f8713314baa5ce254da2fe30f93a98ae651a7aa
David Jones <unixmonkey1@gmail.com>
2015-03-11 19:29:19 -0400

    Drop marker on entered addresses

--- maply.js
+++ maply.js
@@ -2,25 +2,46 @@
 
 var url = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
 
-var start_options = {
-  center: { lat: 39.957139, lng: -86.17521599999999},
+// default options when first displaying the map
+var scotts_house = {
+  center: { lat: 39.957139, lng: -86.17521599999999 },
   zoom: 15,
   mapTypeId: google.maps.MapTypeId.HYBRID
 };
 
-var map = new google.maps.Map($('#map')[0], start_options);
+// create the map
+var map = new google.maps.Map($('#map')[0], scotts_house);
 
+// drop a marker
+new google.maps.Marker({
+  position: scotts_house.center,
+  map: map,
+  title: 'Eleven Fifty Coding Academy'
+});
+
+// watch for form submit
 $('form#geocoder').submit(function(ev){
   ev.preventDefault();
   var address = this.address.value;
-  $.get(url + address).done(function(data){
+
+  // ask Google for the address coordinates
+  $.get(url + address).success(function(data){
     var location = data.results[0].geometry.location;
-    var mapOptions = {
+    var map_options = {
       center: { lat: location.lat, lng: location.lng },
       zoom: 15,
       mapTypeId: google.maps.MapTypeId.HYBRID
     };
-    var map = new google.maps.Map($('#map')[0],
-        mapOptions);
+
+    // move map to new coordinates
+    map.setOptions(map_options);
+
+    // drop marker
+    new google.maps.Marker({
+      position: map_options.center,
+      map: map,
+      title: address
+    });
+
   });
 });
